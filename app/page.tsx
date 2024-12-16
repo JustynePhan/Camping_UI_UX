d3afaa34ae6b5f5fd142043bf272a96e273f455f
Justyne Phan <justynephan@gmail.com>
2024-12-16 15:01:43 -0500

    Fundation of the website

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Camp from "./components/Camp"
+import Hero from "./components/Hero"
+import Guide from "./components/Guide"
+import Features from "./components/Feature"
+import GetApp from "./components/GetApp"
+
+
+export default function Home() {
+  return (
+ <>
+ <Hero />
+ <Camp />
+ <Guide />
+ <Features />
+ <GetApp />
+ 
+ </>
+  )
+}
